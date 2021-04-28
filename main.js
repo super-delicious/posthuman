5044fb96e22eeaede7ffaaf16109637768e1cc1d
super-delicious <61995976+super-delicious@users.noreply.github.com>
2021-04-28 11:45:58 -0700

    updated

--- main.js
+++ main.js
@@ -1,9 +1,139 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import {
+  GLTFLoader
+} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
+import {
+  OrbitControls
+} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+import {
+  RoomEnvironment
+} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/environments/RoomEnvironment.js';
 
-import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+class BasicCharacterControls {
+  constructor(params) {
+    this._Init(params);
+  }
+
+  _Init(params) {
+    this._params = params;
+    this._move = {
+      forward: false,
+      backward: false,
+      left: false,
+      right: false,
+    };
+    this._decceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0);
+    this._acceleration = new THREE.Vector3(1, 0.25, 50.0);
+    this._velocity = new THREE.Vector3(0, 0, 0);
+
+    document.addEventListener('keydown', (e) => this._onKeyDown(e), false);
+    document.addEventListener('keyup', (e) => this._onKeyUp(e), false);
+  }
+
+  _onKeyDown(event) {
+    switch (event.keyCode) {
+      case 87: // w
+        this._move.forward = true;
+        break;
+      case 65: // a
+        this._move.left = true;
+        break;
+      case 83: // s
+        this._move.backward = true;
+        break;
+      case 68: // d
+        this._move.right = true;
+        break;
+      case 38: // up
+      case 37: // left
+      case 40: // down
+      case 39: // right
+        break;
+    }
+  }
+
+  _onKeyUp(event) {
+    switch (event.keyCode) {
+      case 87: // w
+        this._move.forward = false;
+        break;
+      case 65: // a
+        this._move.left = false;
+        break;
+      case 83: // s
+        this._move.backward = false;
+        break;
+      case 68: // d
+        this._move.right = false;
+        break;
+      case 38: // up
+      case 37: // left
+      case 40: // down
+      case 39: // right
+        break;
+    }
+  }
+
+  Update(timeInSeconds) {
+    const velocity = this._velocity;
+    const frameDecceleration = new THREE.Vector3(
+      velocity.x * this._decceleration.x,
+      velocity.y * this._decceleration.y,
+      velocity.z * this._decceleration.z
+    );
+    frameDecceleration.multiplyScalar(timeInSeconds);
+    frameDecceleration.z = Math.sign(frameDecceleration.z) * Math.min(
+      Math.abs(frameDecceleration.z), Math.abs(velocity.z));
+
+    velocity.add(frameDecceleration);
+
+    const controlObject = this._params.target;
+    const _Q = new THREE.Quaternion();
+    const _A = new THREE.Vector3();
+    const _R = controlObject.quaternion.clone();
+
+    if (this._move.forward) {
+      velocity.z += this._acceleration.z * timeInSeconds;
+    }
+    if (this._move.backward) {
+      velocity.z -= this._acceleration.z * timeInSeconds;
+    }
+    if (this._move.left) {
+      _A.set(0, 1, 0);
+      _Q.setFromAxisAngle(_A, Math.PI * timeInSeconds * this._acceleration.y);
+      _R.multiply(_Q);
+    }
+    if (this._move.right) {
+      _A.set(0, 1, 0);
+      _Q.setFromAxisAngle(_A, -Math.PI * timeInSeconds * this._acceleration.y);
+      _R.multiply(_Q);
+    }
+
+    controlObject.quaternion.copy(_R);
+
+    const oldPosition = new THREE.Vector3();
+    oldPosition.copy(controlObject.position);
+
+    const forward = new THREE.Vector3(0, 0, 1);
+    forward.applyQuaternion(controlObject.quaternion);
+    forward.normalize();
+
+    const sideways = new THREE.Vector3(1, 0, 0);
+    sideways.applyQuaternion(controlObject.quaternion);
+    sideways.normalize();
+
+    sideways.multiplyScalar(velocity.x * timeInSeconds);
+    forward.multiplyScalar(velocity.z * timeInSeconds);
+
+    controlObject.position.add(forward);
+    controlObject.position.add(sideways);
+
+    oldPosition.copy(controlObject.position);
+  }
+}
 
 
-class BasicWorldDemo {
+class LoadModel {
   constructor() {
     this._Initialize();
   }
@@ -23,33 +153,40 @@ class BasicWorldDemo {
       this._OnWindowResize();
     }, false);
 
-    const fov = 60;
-    const aspect = 1920 / 1080;
-    const near = 1.0;
-    const far = 1000.0;
-    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    this._camera.position.set(85, 20, 0);
+    //  const fov = 30;
+    //  const aspect = 1920 / 1080;
+    //  const near = 10.0;
+    //  const far = 600.0;
+    this._camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+    this._camera.position.set(-1, -3, 3);
+    //  this._camera.position.set(50, 23, 68);
 
+    //const pmremGenerator = new THREE.PMREMGenerator(Renderer);
+
+
+    // bg color
     this._scene = new THREE.Scene();
+    //this._scene.background = new THREE.Color(0xbfe3dd);
+    //  this._scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture;
 
     let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-    light.position.set(20, 100, 20);
+    light.position.set(20, 100, 10);
     light.target.position.set(0, 0, 0);
     light.castShadow = true;
     light.shadow.bias = -0.001;
     light.shadow.mapSize.width = 2048;
     light.shadow.mapSize.height = 2048;
     light.shadow.camera.near = 0.1;
-    light.shadow.camera.far = 150.0;
+    light.shadow.camera.far = 100.0;
     light.shadow.camera.near = 0.5;
-    light.shadow.camera.far = 150.0;
+    light.shadow.camera.far = 100.0;
     light.shadow.camera.left = 100;
     light.shadow.camera.right = -100;
     light.shadow.camera.top = 100;
     light.shadow.camera.bottom = -100;
     this._scene.add(light);
 
-    light = new THREE.AmbientLight(0x101010);
+    light = new THREE.AmbientLight(0xFFFFFF, 5.0);
     this._scene.add(light);
 
     const controls = new OrbitControls(
@@ -59,43 +196,32 @@ class BasicWorldDemo {
 
     const loader = new THREE.CubeTextureLoader();
     const texture = loader.load([
-        './resources/posx.jpg',
-        './resources/negx.jpg',
-        './resources/posy.jpg',
-        './resources/negy.jpg',
-        './resources/posz.jpg',
-        './resources/negz.jpg',
+      './resources/posx.jpg',
+      './resources/negx.jpg',
+      './resources/posy.jpg',
+      './resources/negy.jpg',
+      './resources/posz.jpg',
+      './resources/negz.jpg',
     ]);
     this._scene.background = texture;
 
-
-
-    const box = new THREE.Mesh(
-      new THREE.BoxGeometry(2, 2, 2),
-      new THREE.MeshStandardMaterial({
-          color: 0xFFFFF,
-      }));
-    box.position.set(0, 1, 0);
-    box.castShadow = true;
-    box.receiveShadow = true;
-    this._scene.add(box);
-
-    for (let x = -1; x < 1; x++) {
-      for (let y = -1; y < 1; y++) {
-        const box = new THREE.Mesh(
-          new THREE.BoxGeometry(6, 6, 10),
-          new THREE.MeshStandardMaterial({
-              color: 0x808080,
-          }));
-        box.position.set(Math.random() + x * 5, Math.random() * 3.0 + 2.0, Math.random() + y * 5);
-        box.castShadow = true;
-        box.receiveShadow = true;
-        this._scene.add(box);
-      }
-    }
-
-
+    this._mixers = [];
+    this._previousRAF = null;
+    this._LoadModel();
     this._RAF();
+  }
+
+
+
+  _LoadModel() {
+    const loader = new GLTFLoader();
+    loader.load('./assets/obj.glb', (gltf) => {
+
+      gltf.scene.traverse(c => {
+        c.castShadow = true;
+      });
+      this._scene.add(gltf.scene);
+    });
   }
 
   _OnWindowResize() {
@@ -105,10 +231,28 @@ class BasicWorldDemo {
   }
 
   _RAF() {
-    requestAnimationFrame(() => {
-      this._threejs.render(this._scene, this._camera);
+    requestAnimationFrame((t) => {
+      if (this._previousRAF === null) {
+        this._previousRAF = t;
+      }
+
       this._RAF();
+
+      this._threejs.render(this._scene, this._camera);
+      this._Step(t - this._previousRAF);
+      this._previousRAF = t;
     });
+  }
+
+  _Step(timeElapsed) {
+    const timeElapsedS = timeElapsed * 0.001;
+    if (this._mixers) {
+      this._mixers.map(m => m.update(timeElapsedS));
+    }
+
+    if (this._controls) {
+      this._controls.Update(timeElapsedS);
+    }
   }
 }
 
@@ -116,5 +260,5 @@ class BasicWorldDemo {
 let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
-  _APP = new BasicWorldDemo();
+  _APP = new LoadModel();
 });
